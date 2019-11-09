f01671dda84056807af1c3c546b591cc547e3748
Joseph Petitti <jppetitti@wpi.edu>
2019-11-09 14:10:07 -0500

    add hero to world

--- static/main.js
+++ static/main.js
@@ -5,6 +5,7 @@ import { drawBoard } from "./game/draw.js";
 import { Enemy } from "./game/enemy.js";
 import { Vector } from "./modules/vector.js";
 import { shuffle, randomInt, hsl } from "./modules/helpers.js";
+import { Hero } from "./game/hero.js";
 
 const blockWidth = 60;
 const blockHeight = 60;
@@ -41,6 +42,13 @@ for (let i = 0; i < 10; i++) {
   );
   enemy.vel = new Vector(0, 10);
   addToWorld(enemy);
+
+  /*
+  const hero = new Hero(new Vector(0, 0).add(
+    new Vector(blockWidth / 2, blockHeight / 2).add(emptySpaces[11])
+  ));
+  addToWorld(hero);
+  */
 }
 
 startUp();
