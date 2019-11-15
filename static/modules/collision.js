d443e22f5c270b04f409ccddc5f64259e393ebce
Matthew Puentes <matt.puentes@gmail.com>
2019-11-15 13:38:13 -0500

    Prettified the code, some comments, disabled debug drawings.

--- static/modules/collision.js
+++ static/modules/collision.js
@@ -1,6 +1,6 @@
 import { Vector } from "./vector.js";
 import { Entity } from "./entity.js";
-import { getTerrain, getDimensions, addToWorld } from "./gamemanager.js";
+import { getTerrain, getDimensions } from "./gamemanager.js";
 
 /**
  * checks if cell position solid (outside is solid)
@@ -9,160 +9,192 @@ import { getTerrain, getDimensions, addToWorld } from "./gamemanager.js";
  * @returns {boolean}
  */
 export function solidAt(i, j) {
-    const board = getTerrain();
-    return (
-      i < 0 ||
-      i >= board.length ||
-      j < 0 ||
-      j >= board[0].length ||
-      board[i][j] !== 0
-    );
+  const board = getTerrain();
+  return (
+    i < 0 ||
+    i >= board.length ||
+    j < 0 ||
+    j >= board[0].length ||
+    board[i][j] !== 0
+  );
 }
 
 /**
  * Returns a list of collision objects to represent the cells the entiity is colliding with.
- * @param {Entity} entity 
+ * @param {Entity} entity
  */
 export function collideWithWorld(entity) {
-    const block = getDimensions();
-    const blockWidth = block.width;
-    const blockHeight = block.height;
+  const block = getDimensions();
+  const blockWidth = block.width;
+  const blockHeight = block.height;
 
-    if (entity.hitsWalls == false)
-        return [];
-    
-    let entity_block_width = 1;
-    let entity_block_height = 1;
+  // TODO: calculate these per-entity.
+  // Entity block width
+  let ebw = 1;
+  // Entity block height
+  let ebh = 1;
 
-    let entityCell = new Vector(Math.floor(entity.pos.x / blockWidth), Math.floor(entity.pos.y / blockHeight));
-    let collidingEntities = [];
+  // Find cell approximation of entity's position
+  let entityCell = new Vector(
+    Math.floor(entity.pos.x / blockWidth),
+    Math.floor(entity.pos.y / blockHeight)
+  );
 
-    // Iterate over any block within collision range of the entity 
-    for (let i = entityCell.x - entity_block_width; i <= entityCell.x + entity_block_width; i++) {
-        for (let j = entityCell.y - entity_block_height; j <= entityCell.y + entity_block_height; j++) {
-            if(solidAt(i, j)){
-                let x = (i + 1) * blockWidth - blockWidth / 2;
-                let y = (j + 1) * blockHeight - blockHeight / 2;
-                let e = new Entity(new Vector(x,y));
-                e.width = blockWidth;
-                e.height = blockHeight;
-                if(solidAt(i + 1, j))
-                    e.collidesRight = false;
-                if(solidAt(i - 1, j))
-                    e.collidesLeft = false;
-                if(solidAt(i, j - 1))
-                    e.collidesTop = false;
-                if(solidAt(i, j + 1))
-                   e.collidesBottom = false;
-                collidingEntities.push(e);
-            }
-        }
+  let collidingEntities = [];
+
+  // Iterate over any block within collision range of the entity
+  for (let i = entityCell.x - ebw; i <= entityCell.x + ebw; i++) {
+    for (let j = entityCell.y - ebh; j <= entityCell.y + ebh; j++) {
+      // If this block is solid, create an entity for it
+      if (solidAt(i, j)) {
+        let x = (i + 1) * blockWidth - blockWidth / 2;
+        let y = (j + 1) * blockHeight - blockHeight / 2;
+        let e = new Entity(new Vector(x, y));
+        e.width = blockWidth;
+        e.height = blockHeight;
+
+        // Determine if this cell needs has neighbors, and thus if it needs collision.
+        if (solidAt(i + 1, j)) e.collidesRight = false;
+        if (solidAt(i - 1, j)) e.collidesLeft = false;
+        if (solidAt(i, j - 1)) e.collidesTop = false;
+        if (solidAt(i, j + 1)) e.collidesBottom = false;
+
+        // If any of it's walls are solid, add the entitiy.
+        if (
+          e.collidesBottom ||
+          e.collidesTop ||
+          e.collidesLeft ||
+          e.collidesRight
+        )
+          collidingEntities.push(e);
+      }
     }
-    return collidingEntities;
+  }
+  return collidingEntities;
 }
 
 /**
  * Determines if two entities are colliding
- * @param {*} entity_a 
- * @param {*} entity_b 
+ * @param {Entity} entityA
+ * @param {Entity} entityB
+ * @returns {boolean}
  */
-export function isColliding(entity_a, entity_b) {
-    const a_left = entity_a.pos.x - entity_a.width / 2;
-    const a_top = entity_a.pos.y - entity_a.height / 2;
-    const b_left = entity_b.pos.x - entity_b.width / 2;
-    const b_top = entity_b.pos.y - entity_b.height / 2;
+export function isColliding(entityA, entityB) {
+  const aLeft = entityA.pos.x - entityA.width / 2;
+  const aTop = entityA.pos.y - entityA.height / 2;
+  const bLeft = entityB.pos.x - entityB.width / 2;
+  const bTop = entityB.pos.y - entityB.height / 2;
 
-    if (a_left + entity_a.width >= b_left &&
-        a_left <= b_left + entity_b.width &&
-        a_top + entity_a.height >= b_top &&
-        a_top <= b_top + entity_b.height) {
-        return true;
-    }
-    return false;
+  if (
+    aLeft + entityA.width >= bLeft &&
+    aLeft <= bLeft + entityB.width &&
+    aTop + entityA.height >= bTop &&
+    aTop <= bTop + entityB.height
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
  * Given two entities A and B, calculates the smallest vector needed to apply to entity A to prevent collision.
  * Returns empty vector if A and B are not colliding.
  * Assumes rectangle A does not surround rectangle B
- * @param {*} entity_a 
- * @param {*} entity_b 
+ * @param {Entity} entityA
+ * @param {Entity} entityB
+ * @returns {Vector}
  */
-export function calculateCollisionVector(entity_a, entity_b){
-    if(!isColliding(entity_a, entity_b))
-        return new Vector(0, 0);
+export function calculateCollisionVector(entityA, entityB) {
+  // If they aren't colliding, the vector is (0,0)
+  // TODO: determine if this is needed. Removing it would be slightly faster.
+  if (!isColliding(entityA, entityB)) return new Vector(0, 0);
 
-    const a_right = entity_a.pos.x + entity_a.width / 2;
-    const a_left = entity_a.pos.x - entity_a.width / 2;
-    const a_bottom = entity_a.pos.y + entity_a.height / 2;
-    const a_top = entity_a.pos.y - entity_a.height / 2;
-    const b_right = entity_b.pos.x + entity_b.width / 2;
-    const b_left = entity_b.pos.x - entity_b.width / 2;
-    const b_bottom = entity_b.pos.y + entity_b.height / 2;
-    const b_top = entity_b.pos.y - entity_b.height / 2;
+  // Define the 4 corners of each bounding rectangle
+  const aRight = entityA.pos.x + entityA.width / 2;
+  const aLeft = entityA.pos.x - entityA.width / 2;
+  const aBottom = entityA.pos.y + entityA.height / 2;
+  const aTop = entityA.pos.y - entityA.height / 2;
+  const bRight = entityB.pos.x + entityB.width / 2;
+  const bLeft = entityB.pos.x - entityB.width / 2;
+  const bBottom = entityB.pos.y + entityB.height / 2;
+  const bTop = entityB.pos.y - entityB.height / 2;
 
-    let collisionVector = new Vector(0, 0);
-    if(b_left < a_right && a_right < b_right){
-        collisionVector.x = a_right - b_left;
-    }
-    if(b_left < a_left && a_left < b_right){
-        collisionVector.x = a_left - b_right;
-    }
-    if(b_top < a_bottom && a_bottom < b_bottom){
-        collisionVector.y = a_bottom - b_top;
-    }
-    if(b_top < a_top && a_top < b_bottom){
-        collisionVector.y = a_top - b_bottom;
-    }
+  let cVector = new Vector(0, 0);
+  // If the right wall of A is between the horizontal walls of B
+  if (bLeft < aRight && aRight < bRight) {
+    cVector.x = aRight - bLeft;
+  }
+  // If the left wall of A is between the horizontal walls of B
+  if (bLeft < aLeft && aLeft < bRight) {
+    cVector.x = aLeft - bRight;
+  }
+  // If the bottom wall of A is between the vertical walls of B
+  if (bTop < aBottom && aBottom < bBottom) {
+    cVector.y = aBottom - bTop;
+  }
+  // If the top wall of A is between the vertical walls of B
+  if (bTop < aTop && aTop < bBottom) {
+    cVector.y = aTop - bBottom;
+  }
 
-    if(!entity_b.collidesBottom)
-        collisionVector.y = Math.max(0, collisionVector.y);
-    if(!entity_b.collidesTop)
-        collisionVector.y = Math.min(0, collisionVector.y);
-    if(!entity_b.collidesLeft)
-        collisionVector.x = Math.min(0, collisionVector.x);
-    if(!entity_b.collidesRight)
-        collisionVector.x = Math.max(0, collisionVector.x);
+  // If the entity doesn't have collision in a direction, make sure it doesn't have the vector point that way.
+  if (!entityB.collidesBottom) cVector.y = Math.max(0, cVector.y);
+  if (!entityB.collidesTop) cVector.y = Math.min(0, cVector.y);
+  if (!entityB.collidesLeft) cVector.x = Math.min(0, cVector.x);
+  if (!entityB.collidesRight) cVector.x = Math.max(0, cVector.x);
 
-    if(Math.abs(collisionVector.x) < Math.abs(collisionVector.y) && collisionVector.x != 0){
-        collisionVector.y = 0
-    } else if(Math.abs(collisionVector.y) < Math.abs(collisionVector.x) && collisionVector.y != 0){
-        collisionVector.x = 0
-    } else {
+  // Only return the "Easier" direction to resolve from.
+  // TODO: figure out why this works?
+  if (Math.abs(cVector.x) < Math.abs(cVector.y) && cVector.x != 0) {
+    cVector.y = 0;
+  } else if (Math.abs(cVector.y) < Math.abs(cVector.x) && cVector.y != 0) {
+    cVector.x = 0;
+  }
 
-    }
-
-    return collisionVector;
+  return cVector;
 }
 
 /**
  * move the entity based on collisions with walls and other entities
- * @param {Entity} entity 
+ * @param {Entity} entity
  */
 export function adjustEntity(entity) {
-    let collidingEntities = collideWithWorld(entity);
-    // TODO: fill this list with entities to collide with.
-    let collisionVectors = [];
-    for(let i = 0; i < collidingEntities.length; i++){
-        const collisionVector = calculateCollisionVector(entity, collidingEntities[i]);
-        if(!(collisionVector.x == 0 && collisionVector.y == 0)){
-            collisionVectors.push(collisionVector);
-        }
-    }
+  // Initialize collision list with collisions between entity and the world
+  let collidingEntities;
+  if (entity.hitsWalls) collidingEntities = collideWithWorld(entity);
+  else collidingEntities = [];
 
-    for(let i = 0; i < collisionVectors.length; i++){
-        if(Math.abs(collisionVectors[i].x) > Math.abs(collisionVectors[i].y) && collisionVectors[i].x != 0){
-            entity.pos.x -= collisionVectors[i].x;
-            entity.vel.x = collisionVectors[i].x * -(entity.bounciness);
-        }
-        else if(Math.abs(collisionVectors[i].y) > Math.abs(collisionVectors[i].x) && collisionVectors[i].y != 0){
-            entity.pos.y -= collisionVectors[i].y;
-            entity.vel.y = collisionVectors[i].y * -(entity.bounciness);
-        } else {
-            entity.pos.x -= collisionVectors[i].x;
-            entity.pos.y -= collisionVectors[i].y;
-            entity.vel.x = collisionVectors[i].x * -(entity.bounciness);
-            entity.vel.y = collisionVectors[i].y * -(entity.bounciness);
-        }
+  // TODO: fill this list with entities to collide with.
+  let collisionVectors = [];
+
+  // Iterate through each colliding entity, and get a vector that defines how "collided" they are
+  for (let i = 0; i < collidingEntities.length; i++) {
+    const collisionVector = calculateCollisionVector(
+      entity,
+      collidingEntities[i]
+    );
+    if (!(collisionVector.x == 0 && collisionVector.y == 0)) {
+      collisionVectors.push(collisionVector);
     }
+  }
+
+  // For each colliding vector, resolve the collision.
+  for (let i = 0; i < collisionVectors.length; i++) {
+    const cv = collisionVectors[i];
+    if (Math.abs(cv.x) > Math.abs(cv.y) && cv.x != 0) {
+      // If x is the "easiest" solution (but not 0), use x.
+      entity.pos.x -= collisionVectors[i].x;
+      entity.vel.x = collisionVectors[i].x * -entity.bounciness;
+    } else if (Math.abs(cv.y) > Math.abs(cv.x) && cv.y != 0) {
+      // If x is the "easiest" solution (but not 0), use x.
+      entity.pos.y -= cv.y;
+      entity.vel.y = cv.y * -entity.bounciness;
+    } else {
+      // If X and Y are equal, resolve them both.
+      entity.pos.x -= cv.x;
+      entity.pos.y -= cv.y;
+      entity.vel.x = cv.x * -entity.bounciness;
+      entity.vel.y = cv.y * -entity.bounciness;
+    }
+  }
 }
