8d77566247843db2440ffb1be384dc07329bc75e
Joseph Petitti <jppetitti@wpi.edu>
2020-01-08 05:24:49 -0500

    Rewrite buttons.js to be more extensible
    Made it much easier to add or remove directional and button controls,
    as well as changing the button.status API to make more sense. Now it
    provides public members indicating whether a button has just been
    pressed, is being held, or was just released. It also handles getting
    controller input more efficiently behind the scenes and abstracts both
    kinds of input (polling for gamepads and listening for keyboard events)
    behind the buttons API.

--- static/game/buttons.js
+++ static/game/buttons.js
@@ -1,41 +1,90 @@
 import { Vector } from "../modules/vector.js";
-import { numSign } from "../modules/helpers.js";
 
 const noisy = false;
 const DEADZONE = 0.2;
+const STICK_SENSITIVITY = 1.4;
 let usingKeyboard = true;
 
 /**
  * @typedef {Object} Status
- * @property {boolean} held
- * @property {boolean} pressed
- * @property {boolean} released
+ * @property {boolean} isDown if the key is down
+ * @property {boolean} wasDown if the key was down last step
+ * @property {boolean} isPressed if the key is down, and was up last step
+ * @property {boolean} isHeld if the key is down, and was down last step
+ * @property {boolean} isReleased if the key is up, and was down last step
  */
 
-/**
- * @typedef {Object} Button
- * @property {string} key
- * @property {Status} status
- */
+class Button {
+  /**
+   * @param {string} key the KeyboardEvent.key value of the key associated with
+   * this button. See here for details:
+   * https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+   * @param {number} [gpButtonIndex] index in gamepad.buttons. Only has effect
+   * if this has type "button"
+   */
+  constructor(key, gpButtonIndex) {
+    this.key = key;
+    this.status = initStatus();
+    this.gpButtonIndex = gpButtonIndex;
+  }
+}
+
+class Directional {
+  /**
+   * @param {string} upKey the KeyboardEvent.key value of the key associated
+   * with going up on this directional
+   * @param {string} rightKey the KeyboardEvent.key value of the key associated
+   * with going right on this directional
+   * @param {string} downKey the KeyboardEvent.key value of the key associated
+   * with going down on this directional
+   * @param {string} leftKey the KeyboardEvent.key value of the key associated
+   * with going left on this directional
+   * @param {number} vAxisIndex index of this directional's vertical axis in
+   * gamepad.axes
+   * @param {number} hAxisIndex index of this directional's horizontal axis
+   * in gamepad.axes
+   */
+  constructor(upKey, rightKey, downKey, leftKey, vAxisIndex, hAxisIndex) {
+    this.up = new Button(upKey);
+    this.right = new Button(rightKey);
+    this.down = new Button(downKey);
+    this.left = new Button(leftKey);
+    this.vAxisIndex = vAxisIndex;
+    this.hAxisIndex = hAxisIndex;
+    this.vec = new Vector(0, 0);
+  }
+
+  /**
+   * Sets this vec for a directional based on what buttons are being pressed
+   */
+  setVecFromButtons() {
+    this.vec = new Vector(0, 0);
+    if (this.left.status.isDown) this.vec.x -= 1;
+    if (this.right.status.isDown) this.vec.x += 1;
+    if (this.up.status.isDown) this.vec.y -= 1;
+    if (this.down.status.isDown) this.vec.y += 1;
+    this.vec = this.vec.norm2();
+  }
+
+  /**
+   * @return {Button[]} an array of this directional's Button objects
+   */
+  getButtons() {
+    return [this.up, this.right, this.down, this.left];
+  }
+}
 
 /**
- * @typedef {Object} Directional
- * @property {Button} up
- * @property {Button} right
- * @property {Button} down
- * @property {Button} left
- * @property {Vector} vec
- */
-
-/**
- * Initializes a sub-object for buttons
+ * Initializes a status to all false
  * @return {Status}
  */
-function initBut() {
+function initStatus() {
   return {
-    held: false,
-    pressed: false,
-    released: false
+    isDown: false,
+    wasDown: false,
+    isPressed: false,
+    isHeld: false,
+    isReleased: false
   };
 }
 
@@ -44,105 +93,89 @@ function initBut() {
  * whether it was just held, pressed, or released
  */
 export const buttons = {
-  /** @type {Directional} */
-  move: {
-    up: { key: "W", status: initBut() },
-    right: { key: "D", status: initBut() },
-    down: { key: "S", status: initBut() },
-    left: { key: "A", status: initBut() },
-    vec: new Vector(0, 0)
-  },
+  move: new Directional("w", "d", "s", "a", 1, 0),
 
-  /** @type {Directional} */
-  shoot: {
-    up: { key: "&", status: initBut() },
-    right: { key: "'", status: initBut() },
-    down: { key: "(", status: initBut() },
-    left: { key: "%", status: initBut() },
-    vec: new Vector(0, 0)
-  },
+  shoot: new Directional(
+    "ArrowUp",
+    "ArrowRight",
+    "ArrowDown",
+    "ArrowLeft",
+    3,
+    2
+  ),
 
   /** @type {Button} */
-  primary: { key: " ", status: initBut() },
+  primary: new Button(" ", 7),
   /** @type {Button} */
-  secondary: { key: "E", status: initBut() }
+  secondary: new Button("e", 6),
+
+  /** @return {Directional[]} */
+  getDirectionals() {
+    return [this.move, this.shoot];
+  },
+
+  /** @return {Button[]} */
+  getButtons() {
+    return [this.primary, this.secondary];
+  },
+
+  *[Symbol.iterator]() {
+    for (const dir of this.getDirectionals()) {
+      for (const b of dir.getButtons()) {
+        yield b;
+      }
+    }
+    for (const button of this.getButtons()) {
+      yield button;
+    }
+  }
 };
 
 /**
- * Sets the vec for a directional based on holds and releases
- * @param {Directional} directional
+ * Let buttons know that a step has just finished
  */
-function calcDirVec(directional) {
-  // make directional vec all 1s and 0s
-  directional.vec.x = numSign(directional.vec.x);
-  directional.vec.y = numSign(directional.vec.y);
-  if (directional.right.status.pressed) {
-    directional.vec.x = 1;
-  }
-  if (directional.left.status.pressed) {
-    directional.vec.x = -1;
-  }
-  if (directional.down.status.pressed) {
-    directional.vec.y = 1;
-  }
-  if (directional.up.status.pressed) {
-    directional.vec.y = -1;
-  }
-
-  // release should stop if the opposite is not held
-  if (directional.right.status.released) {
-    if (directional.left.status.held) {
-      directional.vec.x = -1;
-    } else {
-      directional.vec.x = 0;
-    }
-  }
-  if (directional.left.status.released) {
-    if (directional.right.status.held) {
-      directional.vec.x = 1;
-    } else {
-      directional.vec.x = 0;
-    }
-  }
-  if (directional.down.status.released) {
-    if (directional.up.status.held) {
-      directional.vec.y = -1;
-    } else {
-      directional.vec.y = 0;
-    }
-  }
-  if (directional.up.status.released) {
-    if (directional.down.status.held) {
-      directional.vec.y = 1;
-    } else {
-      directional.vec.y = 0;
-    }
-  }
-  if (!directional.vec.isZeroVec()) {
-    directional.vec = directional.vec.norm();
+export function ageButtons() {
+  for (const button of buttons) {
+    // assume no change
+    button.status.wasDown = button.status.isDown;
+    button.status.isReleased = false;
+    button.status.isHeld = button.status.isDown;
+    button.status.isPressed = false;
   }
 }
 
 /**
- * makes all presses and releases false
+ * Call this when a button has just been pressed
+ * @param {Button} button
  */
-export function cleanButtons() {
-  // set directional button presses and releases to false
-  const directionals = [buttons.move, buttons.shoot];
-  for (let i = 0; i < directionals.length; i++) {
-    const directional = directionals[i];
-    for (const dir in directional) {
-      if (dir !== "vec") {
-        directional[dir].status.pressed = false;
-        directional[dir].status.released = false;
-      }
-    }
-  }
-  // set normal button presses and releases to false
-  buttons.primary.status.pressed = false;
-  buttons.primary.status.released = false;
-  buttons.secondary.status.pressed = false;
-  buttons.secondary.status.released = false;
+function buttonDown(button) {
+  button.status.isDown = true;
+  button.status.isHeld = button.status.wasDown;
+  button.status.isPressed = !button.status.wasDown;
+  button.status.isReleased = false;
+}
+
+/**
+ * Call this when a button has just been released
+ * @param {Button} button
+ */
+function buttonUp(button) {
+  button.status.isDown = false;
+  button.status.isHeld = false;
+  button.status.isPressed = false;
+  button.status.isReleased = button.status.wasDown;
+}
+
+/**
+ * This gets called each step before ageButton() *if* usingKeyboard is false
+ * @param {Button} button
+ * @param {boolean} [currentlyDown = false]
+ */
+function buttonStep(button, currentlyDown = false) {
+  button.status.isDown = currentlyDown;
+  button.status.isHeld = currentlyDown && button.status.wasDown;
+  button.status.isPressed = currentlyDown && !button.status.wasDown;
+  button.status.isReleased = !currentlyDown && button.status.wasDown;
 }
 
 /**
@@ -151,63 +184,25 @@ export function cleanButtons() {
  */
 export function controlKeydownListener(e) {
   usingKeyboard = true;
-  const code = e.keyCode;
-  const key = String.fromCharCode(code);
+  const key = e.key;
 
-  // movement keys
-  for (const dir in buttons.move) {
-    if (dir !== "vec" && key === buttons.move[dir].key) {
+  // is it a stick button?
+  for (const stick of buttons.getDirectionals()) {
+    for (const dir of stick.getButtons()) {
+      if (key === dir.key) {
+        e.preventDefault();
+        buttonDown(dir);
+        stick.setVecFromButtons();
+      }
+    }
+  }
+
+  // is it a normal button?
+  for (const nb of buttons.getButtons()) {
+    if (key === nb.key) {
       e.preventDefault();
-      if (!buttons.move[dir].status.held) {
-        buttons.move[dir].status.pressed = true;
-        if (noisy) {
-          console.log(`move button ${buttons.move[dir].key} pressed`);
-        }
-      }
-      buttons.move[dir].status.held = true;
-      calcDirVec(buttons.move);
-      return;
+      buttonDown(nb);
     }
-  }
-
-  // shooting keys
-  for (const dir in buttons.shoot) {
-    if (dir !== "vec" && key === buttons.shoot[dir].key) {
-      e.preventDefault();
-      if (!buttons.shoot[dir].status.held) {
-        buttons.shoot[dir].status.pressed = true;
-        if (noisy) {
-          console.log(`shoot button ${buttons.shoot[dir].key} pressed`);
-        }
-      }
-      buttons.shoot[dir].status.held = true;
-      calcDirVec(buttons.shoot);
-      return;
-    }
-  }
-
-  // primary and secondary keys
-  if (key === buttons.primary.key) {
-    e.preventDefault();
-    if (!buttons.primary.status.held) {
-      buttons.primary.status.pressed = true;
-      if (noisy) {
-        console.log(`primary button ${buttons.primary.key} pressed`);
-      }
-    }
-    buttons.primary.status.held = true;
-    return;
-  }
-  if (key === buttons.secondary.key) {
-    e.preventDefault();
-    if (!buttons.secondary.status.held) {
-      buttons.secondary.status.pressed = true;
-      if (noisy) {
-        console.log(`secondary button ${buttons.primary.key} pressed`);
-      }
-    }
-    buttons.secondary.status.held = true;
-    return;
   }
 }
 
@@ -217,59 +212,25 @@ export function controlKeydownListener(e) {
  */
 export function controlKeyupListener(e) {
   usingKeyboard = true;
-  const code = e.keyCode;
-  const key = String.fromCharCode(code);
+  const key = e.key;
 
-  // movement keys
-  for (const dir in buttons.move) {
-    e.preventDefault();
-    if (key === buttons.move[dir].key) {
-      buttons.move[dir].status.pressed = false;
-      buttons.move[dir].status.held = false;
-      buttons.move[dir].status.released = true;
-      if (noisy) {
-        console.log(`move button ${buttons.move[dir].key} released`);
+  // is it a stick button?
+  for (const stick of buttons.getDirectionals()) {
+    for (const dir of stick.getButtons()) {
+      if (key === dir.key) {
+        e.preventDefault();
+        buttonUp(dir);
+        stick.setVecFromButtons();
       }
-      calcDirVec(buttons.move);
-      return;
     }
   }
 
-  // shooting keys
-  for (const dir in buttons.shoot) {
-    e.preventDefault();
-    if (key === buttons.shoot[dir].key) {
-      buttons.shoot[dir].status.pressed = false;
-      buttons.shoot[dir].status.held = false;
-      buttons.shoot[dir].status.released = true;
-      if (noisy) {
-        console.log(`shoot button ${buttons.shoot[dir].key} released`);
-      }
-      calcDirVec(buttons.shoot);
-      return;
+  // is it a normal button?
+  for (const nb of buttons.getButtons()) {
+    if (key === nb.key) {
+      e.preventDefault();
+      buttonUp(nb);
     }
-  }
-
-  // primary and secondary keys
-  if (key === buttons.primary.key) {
-    e.preventDefault();
-    buttons.primary.status.pressed = false;
-    buttons.primary.status.held = false;
-    buttons.primary.status.released = true;
-    if (noisy) {
-      console.log(`primary button ${buttons.primary.key} released`);
-    }
-    return;
-  }
-  if (key === buttons.secondary.key) {
-    e.preventDefault();
-    buttons.secondary.status.pressed = false;
-    buttons.secondary.status.held = false;
-    buttons.secondary.status.released = true;
-    if (noisy) {
-      console.log(`secondary button ${buttons.secondary.key} released`);
-    }
-    return;
   }
 }
 
@@ -308,55 +269,59 @@ export function getGamepadInput() {
     if (!gamepad || !gamepad.connected) {
       continue;
     }
-    if (gamepad.axes.length < 4) {
-      continue;
-    }
-    /** @type {number[]} */
-    const sticks = new Array(4);
-    for (let i = 0; i < gamepad.axes.length; ++i) {
-      const num = deadzoneGuard(gamepad.axes[i]);
-      if (num !== 0) usingKeyboard = false; // we're using a controller now
-      if (i < 4) sticks[i] = num; // remember for the first two sticks
-    }
 
-    for (const but of gamepad.buttons) {
-      if (but.value > 0 || but.pressed) {
-        usingKeyboard = false;
+    // check if any of the sticks we care about are pressed
+    for (const dir of buttons.getDirectionals()) {
+      const xai = dir.hAxisIndex;
+      const yai = dir.vAxisIndex;
+      if (xai !== undefined && gamepad.axes[xai]) {
+        const reading = deadzoneGuard(gamepad.axes[xai]);
+        if (reading !== 0) usingKeyboard = false;
+      }
+      if (yai !== undefined && gamepad.axes[yai]) {
+        const reading = deadzoneGuard(gamepad.axes[yai]);
+        if (reading !== 0) usingKeyboard = false;
       }
     }
 
+    // check if any of the buttons we care about are pressed
+    for (const but of buttons.getButtons()) {
+      const bi = but.gpButtonIndex;
+      if (bi !== undefined && gamepad.buttons[bi]) {
+        if (gamepad.buttons[bi].value > 0 || gamepad.buttons[bi].pressed) {
+          usingKeyboard = false;
+        }
+      }
+    }
+
+    // now, if we're not using the keyboard actually get the values from the
+    // controller and assign them to buttons
     if (!usingKeyboard) {
-      const stickSensitivity = 1.4;
-      let moveVec = new Vector(sticks[0], sticks[1]).mult(stickSensitivity);
-      let shootVec = new Vector(sticks[2], sticks[3]).mult(stickSensitivity);
-
-      if (moveVec.mag() > 1) {
-        moveVec = moveVec.norm2();
+      // get values from sticks
+      for (const dir of buttons.getDirectionals()) {
+        const xai = dir.hAxisIndex;
+        const yai = dir.vAxisIndex;
+        let xReading = 0;
+        let yReading = 0;
+        if (xai !== undefined && gamepad.axes[xai]) {
+          xReading = deadzoneGuard(gamepad.axes[xai]);
+        }
+        if (yai !== undefined && gamepad.axes[yai]) {
+          yReading = deadzoneGuard(gamepad.axes[yai]);
+        }
+        dir.vec = new Vector(xReading, yReading).mult(STICK_SENSITIVITY);
+        if (dir.vec.mag() > 1) dir.vec = dir.vec.norm2();
       }
-      if (shootVec.mag() > 1) {
-        shootVec = shootVec.norm2();
-      }
 
-      buttons.move.vec = moveVec;
-      buttons.shoot.vec = shootVec;
-
-      if (gamepad.buttons[6].pressed) {
-        buttons.primary.status.pressed = !buttons.primary.status.held;
-        buttons.primary.status.held = true;
-        buttons.primary.status.released = false;
-      } else {
-        buttons.primary.status.released = buttons.primary.status.held;
-        buttons.primary.status.pressed = false;
-        buttons.primary.status.held = false;
-      }
-      if (gamepad.buttons[7].pressed) {
-        buttons.secondary.status.pressed = !buttons.primary.status.held;
-        buttons.secondary.status.held = true;
-        buttons.primary.status.released = false;
-      } else {
-        buttons.secondary.status.released = buttons.primary.status.held;
-        buttons.secondary.status.pressed = false;
-        buttons.secondary.status.held = false;
+      // get values from buttons
+      for (const but of buttons.getButtons()) {
+        const bi = but.gpButtonIndex;
+        if (bi !== undefined && gamepad.buttons[bi]) {
+          buttonStep(
+            but,
+            gamepad.buttons[bi].value > 0 || gamepad.buttons[bi].pressed
+          );
+        }
       }
     }
   }
