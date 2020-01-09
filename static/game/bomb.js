c8625818155a59cf47b3244d0a2499b23ce9ee59
Joseph Petitti <jppetitti@wpi.edu>
2020-01-09 08:28:56 -0500

    improve look and functionality of bombs
    Bombs now hit enemies twice per second as they explode, not just at the
    end of the explosion. Explosions pass through walls also. Added lots of
    fire particles to the explosion so that it looks better.

--- static/game/bomb.js
+++ static/game/bomb.js
@@ -1,6 +1,8 @@
 import { Entity } from "../modules/entity.js";
 import { Vector } from "../modules/vector.js";
 import { centeredOctagon, centeredCircle } from "./draw.js";
+import { Particle, EffectEnum } from "./particle.js";
+import { addParticle } from "../modules/gamemanager.js";
 
 /**
  * This class represents a bomb that creatures can place in the game world,
@@ -52,10 +54,16 @@ export class Bomb extends Entity {
     this.fuseTime = fuseTime;
     this.onDetonate = new Array();
     this.onBlastCreature = new Array();
-    this.size = 75;
+    this.width = 75;
+    this.height = 75;
     // amount of time (in game steps) it takes to animate exploding
     this.timeToExplode = 20;
     this.blastRadius = 400;
+    /**
+     * @type {Map<string, import("./creature.js").Creature>} use a Map for that
+     * sweet constant time lookup
+     */
+    this.creaturesHit = new Map();
   }
 
   /**
@@ -65,21 +73,15 @@ export class Bomb extends Entity {
     this.fuseTime--;
     if (this.fuseTime === 0) {
       this.detonate();
+    } else if (this.fuseTime < 0 && this.fuseTime >= -1 * this.timeToExplode) {
+      // currently exploding
+      // make this entity bigger for the sake of collisions
+      const radius =
+        (Math.abs(this.fuseTime) / this.timeToExplode) * this.blastRadius;
+      this.width = radius * 2;
+      this.height = radius * 2;
     } else if (this.fuseTime < -1 * this.timeToExplode) {
       // done exploding
-      // make this entity bigger for the sake of collisions
-      this.width = this.blastRadius * 2;
-      this.height = this.blastRadius * 2;
-      // now set the collide map so that the bomb collides only once before
-      // being deleted
-      this.collideMap.set(
-        this.good ? "Enemy" : "Hero",
-        /** @param {import("./creature.js").Creature} creature */ creature => {
-          this.onBlastCreature.map(obj => {
-            obj.func(this, obj.data, creature);
-          });
-        }
-      );
       this.deleteMe = true;
     }
   }
@@ -90,7 +92,14 @@ export class Bomb extends Entity {
   draw() {
     if (this.fuseTime > 0) {
       // draw bomb
-      centeredOctagon(this.drawPos, this.size, this.fillStyle, "white", 4, 0.5);
+      centeredOctagon(
+        this.drawPos,
+        this.width,
+        this.fillStyle,
+        "white",
+        4,
+        0.5
+      );
       // blinking effect
       // TODO see if we can make this look nicer (and get rid of this gross if)
       if (
@@ -105,7 +114,7 @@ export class Bomb extends Entity {
       ) {
         centeredOctagon(
           this.drawPos,
-          this.size,
+          this.width,
           "rgba(255, 255, 255, 0.5)",
           undefined,
           undefined,
@@ -113,20 +122,69 @@ export class Bomb extends Entity {
         );
       }
     } else if (this.fuseTime >= -1 * this.timeToExplode) {
+      // create some fire particles
+      const numParticles = Math.floor(Math.random() * 20) + 6;
+      for (let i = 0; i < numParticles; ++i) {
+        let color;
+        if (Math.random() < 0.1) {
+          color = "hsl(19, 87%, 4%)"; // smoke
+        } else {
+          color = "hsl(" + Math.floor(Math.random() * 55 - 5) + ", 100%, 50%)";
+        }
+        const xOffset = Math.random() * this.width - this.width / 2;
+        const yOffset = Math.random() * this.height - this.height / 2;
+        const p = new Particle(
+          new Vector(this.drawPos.x + xOffset, this.drawPos.y + yOffset),
+          color,
+          EffectEnum.square,
+          5,
+          5,
+          0.08,
+          30,
+          20,
+          //new Vector(Math.random() - 0.5, -0.3)
+          new Vector(0, 0)
+        );
+        const diff = p.drawPos.sub(this.drawPos);
+        let theta = Math.atan(diff.y / diff.x);
+          if (diff.x < 0) theta += Math.PI; // account for left-facing diff
+        const r = 10 + (1 / diff.mag()) + 0.2;
+        p.vel = new Vector(r * Math.cos(theta), r * Math.sin(theta));
+        p.strokeStyle = "white";
+        p.lineWidth = 1;
+        p.width = 20;
+        p.height = 20;
+        addParticle(p);
+      }
       // draw explosion
       const radius =
         (Math.abs(this.fuseTime) / this.timeToExplode) * this.blastRadius;
       const thickness = (radius / this.blastRadius) * 60;
       centeredCircle(this.drawPos, radius, undefined, thickness, "red");
-    }
+    } 
   }
 
   /**
    * makes the bomb go kaboom
    */
   detonate() {
+    this.collidesWithWalls = false;
     for (const od of this.onDetonate) {
       if (od) od.func(this, od.data);
     }
+    this.collideMap.set(
+      this.good ? "Enemy" : "Hero",
+      /** @param {import("./creature.js").Creature} creature */ creature => {
+        // deal damage only every 1/2 second or at the very end of the explosion
+        if (
+          this.fuseTime === -1 * this.timeToExplode ||
+          this.fuseTime % 30 === 0
+        ) {
+          this.onBlastCreature.map(obj => {
+            obj.func(this, obj.data, creature);
+          });
+        }
+      }
+    );
   }
 }
