552ca3758114d45f952171ac2d6ef052a50ff865
bandaloo <bandaloo@icloud.com>
2020-02-14 13:19:14 -0500

    stubbed out methods for refactoring

--- static/modules/displaymanager.js
+++ static/modules/displaymanager.js
@@ -1,3 +1,40 @@
 class DisplayManager {}
 
 const displayManager = new DisplayManager();
+
+/**
+ * adds the display canvas to the div in the DOM with the given id
+ */
+export function addDisplayToDiv(id) {}
+
+/**
+ * returns the draw canvas
+ */
+export function getCanvas() {}
+
+/**
+ * returns the draw context
+ */
+export function getContext() {}
+
+/**
+ * returns the splatter context
+ */
+export function getSplatterContext() {}
+
+/**
+ * returns the draw canvas width
+ */
+export function getCanvasWidth() {}
+
+/**
+ * returns the draw canvas height
+ */
+export function getCanvasHeight() {}
+
+/**
+ * set the additional draw function to happen every game loop (would be
+ * commonly used for drawing terrain)
+ * @param {() => void} drawFunc drawing function to happen every loop
+ */
+export function setGameDrawFunc(drawFunc) {}
