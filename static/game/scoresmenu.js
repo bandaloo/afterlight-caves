a2b3b903e6eabfdf1b2aebca70925d808a8335db
bandaloo <bandaloo@icloud.com>
2020-02-14 14:00:37 -0500

    fixed get width references in scores menu

--- static/game/scoresmenu.js
+++ static/game/scoresmenu.js
@@ -1,12 +1,11 @@
 import {
-  getCanvasWidth,
-  getCanvasHeight,
   getScreenDimensions,
   toggleGuiElement
 } from "../modules/gamemanager.js";
 import { Menu } from "./menu.js";
 import { rect } from "./draw.js";
 import { Vector } from "../modules/vector.js";
+import { getCanvasWidth, getCanvasHeight } from "../modules/displaymanager.js";
 
 export class ScoresMenu extends Menu {
   /**
