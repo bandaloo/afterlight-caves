f1d3ab6c88ea9d99173b9ed50c91141c8f033a17
bandaloo <bandaloo@icloud.com>
2020-02-14 13:36:54 -0500

    added getters and canvas setup to display manager

--- static/modules/displaymanager.js
+++ static/modules/displaymanager.js
@@ -1,40 +1,109 @@
-class DisplayManager {}
+const BLUR_SCALAR = 2;
+
+class DisplayManager {
+  constructor(
+    width = 1920,
+    height = 1080,
+    displayWidth = 960,
+    displayHeight = 540
+  ) {
+    // the canvas for drawing
+    this.canvas = document.createElement("canvas");
+    this.context = this.canvas.getContext("2d");
+    this.canvas.tabIndex = 1;
+    this.canvas.width = width;
+    this.canvas.height = height;
+
+    // the canvas for displaying
+    this.displayCanvas = document.createElement("canvas");
+    this.displayContext = this.displayCanvas.getContext("2d");
+    this.displayWidth = displayWidth;
+    this.displayHeight = displayHeight;
+    this.displayCanvas.width = displayWidth;
+    this.displayCanvas.height = displayHeight;
+
+    // the untouched canvas for blurring before copying
+    this.blurCanvas = document.createElement("canvas");
+    this.blurContext = this.blurCanvas.getContext("2d");
+    this.blurCanvas.width = width / BLUR_SCALAR;
+    this.blurCanvas.height = height / BLUR_SCALAR;
+
+    // the canvas (that is rarely cleared) used for keeping permanent splatter
+    this.splatterCanvas = document.createElement("canvas");
+    this.splatterContext = this.splatterCanvas.getContext("2d");
+
+    this.screenWidth = width;
+    this.screenHeight = height;
+
+    this.resetCounter = 0;
+
+    this.blurContext.filter = "blur(3px) brightness(200%)";
+
+    // drawing func defaults to a no-op
+    this.drawFunc = () => {};
+
+    const exitHandler = () => {
+      if (document.fullscreenElement === null) {
+        this.displayCanvas.width = this.displayWidth;
+        this.displayCanvas.height = this.displayHeight;
+      }
+    };
+
+    this.displayCanvas.addEventListener("fullscreenchange", exitHandler, false);
+  }
+}
 
 const displayManager = new DisplayManager();
 
 /**
  * adds the display canvas to the div in the DOM with the given id
+ * @param {string} id
  */
-export function addDisplayToDiv(id) {}
+export function addDisplayToDiv(id) {
+  const displayDiv = document.getElementById(id);
+  displayDiv.appendChild(displayManager.displayCanvas);
+}
 
 /**
  * returns the draw canvas
  */
-export function getCanvas() {}
+export function getCanvas() {
+  return displayManager.canvas;
+}
 
 /**
  * returns the draw context
  */
-export function getContext() {}
+export function getContext() {
+  return displayManager.context;
+}
 
 /**
  * returns the splatter context
  */
-export function getSplatterContext() {}
+export function getSplatterContext() {
+  return displayManager.splatterContext;
+}
 
 /**
  * returns the draw canvas width
  */
-export function getCanvasWidth() {}
+export function getCanvasWidth() {
+  return displayManager.canvas.width;
+}
 
 /**
  * returns the draw canvas height
  */
-export function getCanvasHeight() {}
+export function getCanvasHeight() {
+  return displayManager.canvas.height;
+}
 
 /**
  * set the additional draw function to happen every game loop (would be
  * commonly used for drawing terrain)
  * @param {() => void} drawFunc drawing function to happen every loop
  */
-export function setGameDrawFunc(drawFunc) {}
+export function setGameDrawFunc(drawFunc) {
+  displayManager.drawFunc = drawFunc;
+}
