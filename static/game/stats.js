7029405f35940a33bef7e0fd93358463f87b4908
bandaloo <bandaloo@icloud.com>
2020-02-14 14:07:25 -0500

    fixed rest of get width references and setGameDrawFunc

--- static/game/stats.js
+++ static/game/stats.js
@@ -1,14 +1,13 @@
 import { Menu } from "./menu.js";
 import { Vector } from "../modules/vector.js";
 import {
-  getCanvasWidth,
-  getCanvasHeight,
   getImportantEntity,
   toggleGuiElement,
   getScreenDimensions
 } from "../modules/gamemanager.js";
 import { rect } from "./draw.js";
 import { Hero } from "./hero.js";
+import { getCanvasWidth, getCanvasHeight } from "../modules/displaymanager.js";
 
 export class Stats extends Menu {
   constructor() {
