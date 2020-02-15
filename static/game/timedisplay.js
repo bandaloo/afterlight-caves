fc2443b0cd0ed8af967faa543c147df6d9bf7ffe
bandaloo <bandaloo@icloud.com>
2020-02-15 13:31:30 -0500

    added pulsing skull

--- static/game/timedisplay.js
+++ static/game/timedisplay.js
@@ -8,8 +8,8 @@ export class TimeDisplay extends GuiElement {
   constructor(pos) {
     super(pos);
     this.borderVec = new Vector(8, 8);
-    this.time = (100 * 60 * 1) / 2; // five minutes
-    //this.time = 100;
+    this.time = 100 * 60 * 5; // five minutes
+    this.skullPulse = 1;
   }
 
   action() {
@@ -18,20 +18,36 @@ export class TimeDisplay extends GuiElement {
     } else {
       /** @type {Hero} */ (getImportantEntity("hero")).takeDamage(Infinity);
     }
+    // exactly on a second
+    if (this.time !== 0 && this.time % 100 === 0) {
+      this.skullPulse = 1;
+    }
+    this.skullPulse *= 0.9;
   }
 
   stepsToTimeString() {
     const minutes = Math.floor(this.time / (60 * 100));
     const seconds = Math.floor((this.time - minutes * 60 * 100) / 100);
-    return `${minutes}:${seconds}`;
+    return `${minutes}:${(seconds + "").padStart(2, "0")}`;
   }
 
   draw() {
+    // draw time
     centeredText(
-      this.stepsToTimeString() + " 💀",
+      this.stepsToTimeString(),
       this.pos.add(this.borderVec),
       "bold 60px anonymous",
       "left",
+      "middle",
+      "white"
+    );
+
+    // draw pulsing skull (the emoji stays)
+    centeredText(
+      "💀",
+      this.pos.add(this.borderVec).add(new Vector(200, 0)),
+      `${Math.floor(this.skullPulse * 15 + 60)}px anonymous`,
+      "center",
       "middle",
       "white"
     );
