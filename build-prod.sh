25e5b3c64b814b00a3407e824ee809e52296a61e
Joseph Petitti <jppetitti@wpi.edu>
2020-02-18 00:50:51 -0500

    copy font file to dist directory

--- build-prod.sh
+++ build-prod.sh
@@ -5,6 +5,7 @@ node_modules/.bin/browserify -vd build/main.js -o dist/bundle.js
 cp static/index.html dist/
 cp static/style.css dist/
 cp static/license.txt dist/
+cp static/anonymous-pro-b.ttf dist/
 sed -i 's/<script type="module" src="main.js"/<script src="bundle.js"/g' dist/index.html
 cp static/sounds dist/sounds -r
 rm -r build
