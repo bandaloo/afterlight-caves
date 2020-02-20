2014145cdc610f38ff9d5ea80e4c36a545871000
bandaloo <bandaloo@icloud.com>
2020-02-19 22:18:07 -0500

    got rid of completed todo

--- static/game/shooter.js
+++ static/game/shooter.js
@@ -63,7 +63,6 @@ export class Shooter extends Enemy {
 
   action() {
     super.action();
-    // TODO make this AI better
     if (hasImportantEntity("hero")) {
       const hero = getImportantEntity("hero");
       if (Math.random() < 0.01) {
