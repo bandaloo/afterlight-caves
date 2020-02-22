9c8b34fa37b3141b864a5bd2bce42c0439f3b29f
Joseph Petitti <jppetitti@wpi.edu>
2020-02-21 20:21:46 -0500

    add --posix to sed command

--- build-prod.sh
+++ build-prod.sh
@@ -5,6 +5,6 @@ cp static/index.html dist/
 cp static/style.css dist/
 cp static/license.txt dist/
 cp static/anonymous-pro-b.ttf dist/
-sed -i 's/<script type="module" src="main.js"/<script src="bundle.js"/g' dist/index.html
+sed -i --posix 's/<script type="module" src="main.js"/<script src="bundle.js"/g' dist/index.html
 cp static/sounds dist/sounds -r
 rm -r build
