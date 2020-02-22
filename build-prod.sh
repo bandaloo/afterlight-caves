9f59c047f8b05ada00d63c7c36c8579e87bbb7eb
Joseph Petitti <jppetitti@wpi.edu>
2020-02-21 20:25:40 -0500

    comply with MacOS cp

--- build-prod.sh
+++ build-prod.sh
@@ -6,5 +6,5 @@ cp static/style.css dist/
 cp static/license.txt dist/
 cp static/anonymous-pro-b.ttf dist/
 sed -i --posix 's/<script type="module" src="main.js"/<script src="bundle.js"/g' dist/index.html
-cp static/sounds dist/sounds -r
+cp -r static/sounds dist/sounds
 rm -r build
