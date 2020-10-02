09794e911a05c90d11d46b4e76ce3fa8dba94231
Joseph Petitti <jppetitti@wpi.edu>
2020-10-01 22:48:48 -0400

    write new scripts to build with electron

--- electron.js
+++ electron.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 const serve = require('electron-serve');
 
 const loadURL = serve({directory: 'dist'});
@@ -6,19 +7,21 @@ const loadURL = serve({directory: 'dist'});
 let mainWindow;
 
 (async () => {
-	await app.whenReady();
+  await app.whenReady();
 
-	mainWindow = new BrowserWindow({
+  app.allowRendererProcessReuse = false;
+
+  mainWindow = new BrowserWindow({
       width: 964,
-      height: 544,
+      height: 570,
       webPreferences: {
         nodeIntegration: true
       }
     });
 
-	await loadURL(mainWindow);
+  await loadURL(mainWindow);
 
-	// The above is equivalent to this:
-	await mainWindow.loadURL('app://-');
-	// The `-` is just the required hostname
+  // The above is equivalent to this:
+  await mainWindow.loadURL('app://-');
+  // The `-` is just the required hostname
 })();
