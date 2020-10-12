02f137e0833bf448125d30cce1070b39da63e22c
Joseph Petitti <jppetitti@wpi.edu>
2020-10-12 17:02:33 -0400

    get scores from server in desktop version
    Environment variables were not being remembered between scripts because
    they weren't exported

--- build-electron.sh
+++ build-electron.sh
@@ -2,8 +2,8 @@
 
 # Change these if you want to run the score server on a different domain from
 # the main content.
-SCORE_SERVER_SCHEME=https
-SCORE_SERVER_DOMAIN=afterlightcaves.com
+export SCORE_SERVER_SCHEME=https
+export SCORE_SERVER_DOMAIN=afterlightcaves.com
 
 ./build-prod.sh
 
