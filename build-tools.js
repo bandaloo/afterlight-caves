9e04364c964a03d2fd7432640b65d7c203cb69ff
Joseph Petitti <jppetitti@wpi.edu>
2020-10-24 15:29:29 -0400

    replace npm package with npm-run

--- build-tools.js
+++ build-tools.js
@@ -1,16 +1,17 @@
-const npm = require("npm");
+const npmRun = require("npm-run");
 const fs = require("fs").promises;
 const fse = require("fs-extra");
 const path = require("path");
 
 /**
- * wraps npm.run() in a function that returns a promise to avoid callback hell
+ * wraps npmRun.run() in a function that has no options and returns a promise to
+ * avoid callback hell
  * @param{string} script the npm script to run
  * @return{Promise<void>}
  */
 const runPromise = (script) => {
   return new Promise((resolve) => {
-    npm.run(script, () => {
+    npmRun.exec(script, {}, () => {
       resolve();
     });
   });
@@ -37,31 +38,29 @@ const replaceInFile = (path, find, replace) => {
  */
 const buildProd = (scoreServerScheme = undefined, scoreServerDomain = undefined) => {
   return new Promise((resolve) => {
-    npm.load(() => {
-      runPromise("compile-compat")
-        .then(() => runPromise("browserify-compat"))
-        .then(() => fse.copy(path.join("static", "index.html"), path.join("dist", "index.html")))
-        .then(() => fse.copy(path.join("static", "style.css"), path.join("dist", "style.css")))
-        .then(() => fse.copy(path.join("static", "license.txt"), path.join("dist", "license.txt")))
-        .then(() => fse.copy(path.join("static", "favicon.ico"), path.join("dist", "favicon.ico")))
-        .then(() => fse.copy(path.join("static", "anonymous-pro-b.ttf"), path.join("dist", "anonymous-pro-b.ttf")))
-        .then(() => replaceInFile(path.join("dist", "index.html"), /<script type="module" src="main.js"/, "<script src=\"bundle.js\""))
-        .then(() => {
-          if (scoreServerScheme !== undefined && scoreServerDomain !== undefined) {
-            return replaceInFile(path.join("dist", "bundle.js"), /var GAME_URL = ".*";/, `var GAME_URL = "${scoreServerScheme}://${scoreServerDomain}";`);
-          } else {
-            return Promise.resolve();
-          }
-        })
-        .then(() => replaceInFile(path.join("dist", "bundle.js"), /\.\./g, "."))
-        .then(() => fse.copy(path.join("static", "sounds"), path.join("dist", "sounds")))
-        .then(() => fse.copy(path.join("static", "images"), path.join("dist", "images")))
-        .then(resolve)
-        .catch((reason) => {
-          console.error("Error encountered in buildProd:");
-          console.error(reason);
-        });
-    });
+    runPromise("babel static --out-dir build")
+      .then(() => runPromise("browserify -vd build/main.js -o dist/bundle.js"))
+      .then(() => fse.copy(path.join("static", "index.html"), path.join("dist", "index.html")))
+      .then(() => fse.copy(path.join("static", "style.css"), path.join("dist", "style.css")))
+      .then(() => fse.copy(path.join("static", "license.txt"), path.join("dist", "license.txt")))
+      .then(() => fse.copy(path.join("static", "favicon.ico"), path.join("dist", "favicon.ico")))
+      .then(() => fse.copy(path.join("static", "anonymous-pro-b.ttf"), path.join("dist", "anonymous-pro-b.ttf")))
+      .then(() => replaceInFile(path.join("dist", "index.html"), /<script type="module" src="main.js"/, "<script src=\"bundle.js\""))
+      .then(() => {
+        if (scoreServerScheme !== undefined && scoreServerDomain !== undefined) {
+          return replaceInFile(path.join("dist", "bundle.js"), /var GAME_URL = ".*";/, `var GAME_URL = "${scoreServerScheme}://${scoreServerDomain}";`);
+        } else {
+          return Promise.resolve();
+        }
+      })
+      .then(() => replaceInFile(path.join("dist", "bundle.js"), /\.\./g, "."))
+      .then(() => fse.copy(path.join("static", "sounds"), path.join("dist", "sounds")))
+      .then(() => fse.copy(path.join("static", "images"), path.join("dist", "images")))
+      .then(resolve)
+      .catch((reason) => {
+        console.error("Error encountered in buildProd:");
+        console.error(reason);
+      });
   });
 };
 
